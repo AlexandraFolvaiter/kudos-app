aab0facbb54a4ed30b79cb88e4aac8c6fc874cdf
Alexandra Folvaiter-Tudose <folvaiter.alexandra@gmail.com>
2022-10-21 11:02:04 +0300

    Added code

--- src/index.jsx
+++ src/index.jsx
@@ -1,57 +1,163 @@
-import ForgeUI, { render, Fragment, Text, GlobalPage, Form, TextField, TextArea, Heading, Select, Option } from '@forge/ui';
+import ForgeUI, { useState, render, Fragment, Text, GlobalPage, Form, TextField, Image, Heading, Select, Option, UserPicker, Table, Row, Cell, Button, ButtonSet, TextArea } from '@forge/ui';
+import { fetch } from '@forge/api';
+import api, { route } from "@forge/api";
 
 const App = () => {
-  var kudosIsSent = false;
+  const [formState, setFormState] = useState(undefined);
+  const [stage, setStage] = useState(0);
+  const [userDetails, setUserDetails] = useState(undefined);
 
-  if (kudosIsSent) {
-    return (<Text>
-      The kudos to ... is sent! Send another one!
-    </Text>)
-  } else {
-    return (
-      <Fragment>
-        <Fragment style="color: red;">
-          <Heading size="large">
-            Kudos App
-          </Heading>
-        </Fragment>
-        <Form onSubmit={() => sendKudos()} submitButtonAppearance="primary" submitButtonText="Send kudos">
-          <Heading size="medium">
-            To
-          </Heading>
-          <Select name="to">
-            <Option label="Milestone 1" value="one" />
-            <Option label="Milestone 2" value="two" />
-            <Option label="Milestone 3" value="three" />
-          </Select>
+  const onSubmit = async (formData) => {
+    setFormState(formData);
+    setStage(1);
 
-          <Heading size="medium">
-            Image
-          </Heading>
-          <Select name="image">
-            <Option label="Milestone 1" value="one" />
-            <Option label="Milestone 2" value="two" />
-            <Option label="Milestone 3" value="three" />
-          </Select>
+    const userDetailsResponse = await api.asApp().requestConfluence(route`/wiki/rest/api/user?accountId=${formData.user}`, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(response => response.json());
 
-          <Heading size="medium">
-            Message
-          </Heading>
-          <TextArea name="message"></TextArea>
+    setUserDetails(userDetailsResponse)
+  };
 
-          <Heading size="medium">
-            Sign as
-          </Heading>
-          <Select name="sign-as">
-            <Option defaultSelected label="Anonym" value="anonym" />
-            {/* todo: add the name of the employee */}
-            <Option label="My identity" value="self" />
-          </Select>
+  const sendKudos = async () => {
 
-        </Form>
-      </Fragment>
+    var toEmail = userDetails?.email;
+    var fromEmail = "hexa.store.test@gmail.com";
+    var subject = "A colleague has something to say to you! Kudos!"
+    var body = `Hi ${userDetails.displayName},
+
+    <p>
+    A colleague has something to say to you! 
+    </p>
+    <h2>${formState.message}</h2>
+    
+    <img width=\\"400px\\" src=\\"${formState.image}\\" />
+
+    <br/><br/>
+    
+    Sent from KudosApp.
+    `;
+
+    await fetch(
+      "https://emailservice20221015082156.azurewebsites.net/api/emails",
+      {
+        method: "POST",
+        body: `
+        {
+          "to": "${toEmail}",
+          "from": "${fromEmail}",
+          "server": "smtp.sendgrid.net",
+          "port": 587,
+          "username": "apikey",
+          "password": "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",
+          "subject": "${subject}",
+          "body": "${body}"
+        }`
+      }
     );
+
+    setStage(2);
   }
+
+  const goToFirstStep = (resetForm = false) => {
+    setStage(0);
+    if (resetForm == true) {
+      setFormState(undefined);
+    }
+  }
+
+  return (
+    <Fragment>
+      <Heading size="large">
+        Kudos App
+      </Heading>
+      <Table>
+        <Row>
+          <Cell>
+
+          </Cell>
+          <Cell >
+
+            {stage == 0 &&
+              <Fragment>
+                <Heading size="medium">
+                  Add details
+                </Heading>
+                <Form onSubmit={async (args) => await onSubmit(args)} submitButtonAppearance="primary" submitButtonText="Preview & Send kudos">
+
+                  <UserPicker label="User" name="user" isRequired="true" includeUsers="true" defaultValue={formState?.user ?? ''} />
+
+                  <Select label="Image" name="image" isRequired="true" defaultValue={formState?.image ?? ''}>
+                    <Option label="Congrats 1" value="https://i.im.ge/2022/10/17/2hql4K.1.png" />
+                    <Option label="Congrats 2" value="https://i.im.ge/2022/10/17/2hqTTz.2.png" />
+                    <Option label="Congrats 3" value="https://i.im.ge/2022/10/17/2hqus9.3.png" />
+                    <Option label="Happy Birthday 1" value="https://i.im.ge/2022/10/17/2hqSmM.4.png" />
+                    <Option label="Happy Birthday 2" value="https://i.im.ge/2022/10/17/2hqdWY.5.png" />
+                    <Option label="Encouragement" value="https://i.im.ge/2022/10/17/2hqaU4.6.png" />
+                    <Option label="I am grateful" value="https://i.im.ge/2022/10/17/2hq7YC.7.png" />
+                  </Select>
+
+                  <TextArea label="Message" name="message" defaultValue={formState?.message ?? ''} isRequired="true" />
+
+                </Form>
+              </Fragment>
+            }
+
+            {
+              stage == 1 && <Fragment>
+                <Heading size="medium">
+                  Preview
+                </Heading>
+                <Heading>
+                  Send to: {userDetails?.displayName}
+                </Heading>
+
+                <Heading>
+                  {formState.message}
+                </Heading>
+
+                <Image
+                  src={formState.image}
+                  alt="Image"
+                  size="xlarge"
+                />
+
+
+                <Fragment>
+                  <ButtonSet>
+                    <Button text="Back" onClick={() => goToFirstStep()} appearance="subtle" />
+                    <Button text="Send kudos" onClick={() => sendKudos()} appearance="primary" />
+                  </ButtonSet>
+                </Fragment>
+
+              </Fragment>
+            }
+
+            {stage == 2 &&
+              <Fragment>
+
+                <Heading>
+                  Congrats! You sent a kudos!
+                </Heading>
+                <Image
+                  src="https://i.im.ge/2022/10/21/2NuZv4.Green-and-White-Modern-Congrats-On-a-Great-Job-Card.gif"
+                  alt="congrats-image"
+                  size="xlarge" />
+
+
+                <Button text="Start over" onClick={() => goToFirstStep(true)} appearance="primary" />
+              </Fragment>
+            }
+
+          </Cell>
+          <Cell>
+          </Cell>
+        </Row>
+      </Table>
+
+    </Fragment>
+  );
 };
 
 export const run = render(
@@ -59,8 +165,3 @@ export const run = render(
     <App />
   </GlobalPage>
 );
-
-function sendKudos() {
-  // do something
-  sendKudos = true;
-}
